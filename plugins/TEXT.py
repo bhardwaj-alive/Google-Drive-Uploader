172e885619e25a815e6ea64f32ba90e8e0bc629b
bhardwaj-alive <70652841+bhardwaj-alive@users.noreply.github.com>
2020-09-10 22:37:42 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -42,7 +42,7 @@ REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
 ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n"
 AUTH_URL = '<a href ="{}">Visit This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
-UPLOADING = "Download Complete ...❤️\n Uploading Your file"
+UPLOADING = "Download Complete \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
@@ -55,10 +55,10 @@ MEGA = True
 
 UPDATE = """ ImpRoved InterFace """
 
-            ❤️ Direct Links 
-            💔 Yt-Dl supported links
-            💔 Openload links
-            ❤️ Dropbox links 
-            💔 Mega links (only files)
-            ❤️ Streaming Links 
-            🐒 Ip specific links
+            Direct Links 
+            Some Yt-Dl supported links
+            Openload links
+            Dropbox links 
+            Mega links (only files)
+            Streaming Links 
+            No Ip specific links please
